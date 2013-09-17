1d09e20402a1cad1139911b0b2cf20a862fedb13
Thomas Davis <thomasalwyndavis@gmail.com>
2013-09-17 15:45:59 +1000

    server list

---
+++ server.js
@@ -0,0 +1,52 @@
+var MailChimpAPI = require('mailchimp').MailChimpAPI;
+//
+var apiKey = process.env.MAILCHIMP_API_KEY;
+
+try { 
+    var api = new MailChimpAPI(apiKey, { version : '2.0' });
+} catch (error) {
+    console.log(error.message);
+}
+
+
+
+var express = require('express');
+
+var port = process.env.PORT || 8080;
+
+var allowCrossDomain = function(req, res, next) {
+  var allowedHost = [
+    'http://2.stopwatching.us',
+    'http://localhost:4000'
+  ];
+  if(allowedHost.indexOf(req.headers.origin) !== -1 ) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin)
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    next();
+  } else {
+    res.send({auth: false});
+  }
+}
+
+var server = express();
+server.use(express.bodyParser());
+
+server.options("*", function(req,res,next){res.send({});});
+
+server.get('/', function(req,res,next){
+    api.call('lists', 'subscribe', { id: 'c05d6bd75f', email: {email:'thomasalwyndavis+wwwwwaa@gmail.com'}, double_optin: false }, function (error, data) {
+    if (error)
+        console.log(error.message);
+    else
+        res.send(JSON.stringify(data)); // Do something with your data!
+});
+});
+
+
+
+  server.listen(port, function() {
+    console.log('%s listening at %s', server.name, server.url);
+  });
+
